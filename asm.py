4137a14bfe401817e4361c76ca7ea170c1a769ff
Jiyi <jiyixx@gmail.com>
2019-02-21 14:55:42 +0800

    添加注释

--- asm.py
+++ asm.py
@@ -3,20 +3,22 @@ import base64
 import time
 import configparser
 
+# 读取配置文件
 cfg = configparser.ConfigParser()
 cfg.read('config.ini')
-
 hostname = cfg.get('user', 'hostname')
 username = cfg.get('user', 'username')
 password = cfg.get('user', 'password')
-# deviceId = '70378'
 ip = cfg.get('user', 'ip')
 mac = cfg.get('user', 'mac')
 
+# 字符串转base64编码，用于转换用户名和密码
 def str_base64(string):
   return bytes.decode(base64.b64encode(str.encode(string)))
 
+# 字符串转字典型，用于转换从服务器得到的结果
 def str_dict(string):
+  # 在eval()中用到以下两个变量
   length = 'length'
   def parseInt(num):
     return int(num)
@@ -24,6 +26,7 @@ def str_dict(string):
   sdict = string[string.index(r'{'):]
   return eval(sdict)
 
+# 创建xml字符串用于提交查询信息
 def create_dev_xml(ip, mac):
   dev_xml = []
   dev_xml.append(r'<?xml version="1.0" encoding="gb2312"?><MSAC><DeviceName>MSHOME\jiy</DeviceName><OS>MACVersion 10.14.3 (Build 18D109)</OS><Ip>')
@@ -33,6 +36,7 @@ def create_dev_xml(ip, mac):
   dev_xml.append(r'</Mac><IeVersion>Safari</IeVersion><Mark>255.255.255.0</Mark><GateWay></GateWay><DiskId>49C8BE1C7BC45BE9A4846F61AD9D7CEA</DiskId></MSAC>')
   return ''.join(dev_xml)
 
+# 查询信息
 getDeviceInfo = {
   'tradecode': 'getdeviceinfoprocess',
   'gettype': 'control',
@@ -48,9 +52,7 @@ getDeviceInfo = {
   'os_platform': 'MAC'
 }
 
-# def get_deviceId():
-#   return deviceId
-
+# 登陆信息
 def get_netAuth(deviceId):
   return {
   'tradecode': 'net_auth',
